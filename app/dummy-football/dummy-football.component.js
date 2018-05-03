0333328726667d1a7341f8f88baa89dedbb351bc
Hamza Ibrahim <hamzaibrahim@Hamzas-MacBook-Pro.local>
2018-05-03 15:04:17 +0100

    able to display added players to array (ng-repeat)in view

--- app/dummy-football/dummy-football.component.js
+++ app/dummy-football/dummy-football.component.js
@@ -1,8 +1,20 @@
 class DummyFootballCtrl {
   constructor($http) {
     this.$http = $http;
-    this.team = [];
+    this.team = {
+      keeper: [],
+      defenders: [],
+      midfielders: [],
+      forwards: []
+    };
+    this.positions = {
+      keeper: ['Keeper'],
+      defenders: ['Centre-Back', 'Left-Back', 'Right-Back'],
+      midfielders: ['Left Wing', 'Right Wing', 'Left Midfield', 'Right Midfield', 'Defensive Midfield', 'Central Midfield', 'Attacking Midfield'],
+      forwards:['Centre-Forward', 'Secondary Striker']
+    }
   }
+
   getPremierLeague() {
     this.$http.get('premier-league/premier.json').then((response) => {
       console.log(this.players = response.data);
@@ -10,14 +22,50 @@ class DummyFootballCtrl {
   };
 
   addPlayer(player) {
-    this.team.push(player);
-    console.log('data inside parent: ', player, 'team', this.team);
+    console.log(player.position)
+    // if (player.position == this.positions.keeper) {
+
+    // } else if (player.position == this.positions.defenders.includes(player.positions)) {
+    //   this.team.defenders.push(player);
+    // }
+    if (this.positions.keeper.includes(player.position)) {
+      if (this.team.keeper.length < 1) {
+        this.team.keeper.push(player);
+      } else {
+        console.log('Selected 1 keeper already');
+      }
+    }
+    if (this.positions.defenders.includes(player.position)) {
+      if (this.team.defenders.length < 4) {
+        this.team.defenders.push(player);
+      } else {
+        console.log('Selected 4 defenders already');
+      }
+    }
+    if (this.positions.midfielders.includes(player.position)) {
+      if (this.team.midfielders.length < 4) {
+        this.team.midfielders.push(player);
+      } else {
+        console.log('Selected 4 midfielders already');
+      }
+    }
+    if (this.positions.forwards.includes(player.position)) {
+      if (this.team.forwards.length < 2) {
+        this.team.forwards.push(player);
+      } else {
+        console.log('Selected 2 forwards already');
+      }
+    }
+
+      // this.team.push(player);
+    // console.log('data inside parent: ', player, 'team', this.team);
   }
 
   removePlayer(player) {
-    let playerIndex = this.team.indexOf(player);
-
-    this.team = [...this.team.slice(0, playerIndex), ...this.team.slice(playerIndex + 1)]
+    let playerIndexKeeper = this.team.keeper.indexOf(player);
+    let playerIndexDefenders = this.team.defenders.indexOf(player);
+    this.team.keeper = [...this.team.keeper.slice(0, playerIndexKeeper), ...this.team.keeper.slice(playerIndexKeeper + 1)]
+    this.team.defenders = [...this.team.defenders.slice(0, playerIndexDefenders), ...this.team.defenders.slice(playerIndexDefenders + 1)]
   }
 }
 
